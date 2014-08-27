2cefcbc492bcbabf77189ad11b4415ee8bf1c321
Justin Rolston <justinrolston@gmail.com>
2014-08-26 20:48:42 -0400

    add 15 ruby file

---
+++ 15.rb
@@ -0,0 +1,10 @@
+=begin
+
+Chapter 15. Use Modules as Name Spaces
+
+* A Ruby module is the container part of a class without the factory.
+* You can’t instantiate a module, but you can put things inside of a module.
+* Modules can hold methods, constants, classes, and even other modules.
+
+=end
+
